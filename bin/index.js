9214b5f6ed6ad0785ba1cd564a375bdda317420f
Alexander Kizilov <kizilov.alex1823@gmail.com>
2022-10-17 18:12:43 +0300

    ...

--- bin/index.js
+++ bin/index.js
@@ -7,7 +7,14 @@ const rawArgs = process.argv.slice(2)
 
 if (rawArgs.includes('init')) {
   copyConfigs()
-  exec('npm i dotenv express')
+  exec('npm i dotenv express', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  })
 } else if (rawArgs.includes('test')) {
   console.log('test');
 }
