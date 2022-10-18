e6278a19bf6bb65b330e91c167704b270d6d3276
Alexander Kizilov <kizilov.alex1823@gmail.com>
2022-10-18 13:37:11 +0300

    ...

--- commands/makeDir.js
+++ commands/makeDir.js
@@ -1,34 +1,24 @@
 const fs = require('fs')
 const path = require('path')
 
-const makeDir = (dirPath) => {
-  console.log(dirPath);
-  const dirs = dirPath.split(/[\/\\]/g)
-  console.log(dirs);
-
-  const createDir = (dirName) => {
-    if (!fs.existsSync(path.join(dirName))) {
-      fs.mkdirSync(path.join(dirName))
-    }
+const createDir = (dirName) => {
+  if (!fs.existsSync(path.join(dirName))) {
+    fs.mkdirSync(path.join(dirName))
   }
+}
+
+const makeDir = (dirPath) => {
+  const dirs = dirPath.split(/[\/\\]/g)
 
   if (dirs.length === 1) {
     createDir(dirs[0])
   } else if (dirs.length > 1) {
     for (let idx = 0; idx < dirs.length; idx++) {
-      // const dir = dirs[idx];
       createDir(path.join(...dirs.slice(0, idx + 1)))
     }
   } else {
     console.error('Directory was not set');
   }
-
-  // for (let dir in source) {
-  //   if (dir === 'toString') continue
-  //   if (!fs.existsSync(path.join(source[dir]))) {
-  //     fs.mkdirSync(path.join(source[dir]))
-  //   }
-  // }
 }
 
 exports.makeDir = makeDir
