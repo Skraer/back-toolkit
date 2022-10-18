46a58a8788e0556dfad268da06535066cf3a24a5
Alexander Kizilov <kizilov.alex1823@gmail.com>
2022-10-18 15:35:28 +0300

    ...

--- commands/utils/copyFile.js
+++ commands/utils/copyFile.js
@@ -2,13 +2,10 @@ const fs = require('fs')
 const path = require('path')
 
 const copyFile = (fromPath, toPath) => {
-
-  const src = path.join(__dirname, '../', fromPath)
+  const src = path.join(__dirname, '../../', fromPath)
   const output = path.join(process.cwd(), '/', toPath)
-  const testPath = path.join('/')
   console.error('src: ', src);
   console.error('output: ', output);
-  console.error('testPath: ', testPath);
 
   const rawData = fs.readFileSync(path.join(src))
   // const nodemonConfig = fs.readFileSync(path.join(configPaths, 'nodemon.json'))
