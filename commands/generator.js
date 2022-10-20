e42caa0bba8951fbaf8c2baa6e517c53e6fc9410
Alexander Kizilov <kizilov.alex1823@gmail.com>
2022-10-20 18:03:13 +0300

    ...

--- commands/generator.js
+++ commands/generator.js
@@ -27,7 +27,7 @@ const writeFileTo = (pathTo, textData) => {
   const dirs = pathTo.split(/[\/\\]/g)
   console.log('FIRST', dirs.slice(0, -1));
   console.log('SECOND', dirs.slice(0, dirs.length - 1));
-  console.log('THIRD', pathTo.split(/[\/\\]/g).slice(0, -1));
+  console.log('THIRD', pathTo.split(/[\/\\]/g).slice(0, -1).join('/') + '/');
 
   console.log('==========');
   // fs.writeFileSync(
