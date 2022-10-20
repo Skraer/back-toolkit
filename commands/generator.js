014050c414d7136cc0dae5d06ac82839c1f707da
Alexander Kizilov <kizilov.alex1823@gmail.com>
2022-10-20 17:14:04 +0300

    ...

--- commands/generator.js
+++ commands/generator.js
@@ -6,37 +6,82 @@ const { config, replaceTemplate } = require('../utils/template')
 
 const rawArgs = process.argv.slice(2)
 
+const getTplText = (fileName) =>
+  fs.readFileSync(path.join(paths.root, 'src', '_templates', fileName)).toString()
+
+const replaceAllTemplates = (textData, input) =>
+  textData.replace(config.patternRegExp, (str) => replaceTemplate(str, input))
+
+const replaceBlocksWithMongo = (textData) => {
+  return textData.replace(
+    config.patternBlockMongoRegExp,
+    (str, content) => replaceTemplate(str, rawArgs.includes('-mongo') ? content : '')
+  )
+}
+
+const writeFileTo = (pathTo, textData) => {
+  fs.writeFileSync(
+    path.join(pathTo),
+    Buffer.from(textData.trim())
+  )
+}
+
 const generateController = () => {
   const name = getArgAfter('/c')
   const fileName = name[0].toUpperCase() + name.slice(1).toLowerCase() + 'Controller.ts'
 
-  const textData = fs.readFileSync(path.join(paths.root, 'src', '_templates', 'controller.ts')).toString()
-  let resultTextData = textData.replace(config.patternRegExp, (str) => replaceTemplate(str, name))
+  const textData = getTplText('controller.ts')
+  let resultTextData = replaceAllTemplates(textData, name)
+  // let resultTextData = textData.replace(config.patternRegExp, (str) => replaceTemplate(str, name))
 
-  fs.writeFileSync(
+  writeFileTo(
     path.join(paths.execRoot, 'src', 'controllers', fileName),
-    Buffer.from(resultTextData.trim())
+    resultTextData
   )
+  // fs.writeFileSync(
+  //   path.join(paths.execRoot, 'src', 'controllers', fileName),
+  //   Buffer.from(resultTextData.trim())
+  // )
 }
 
 const generateModel = () => {
   const name = getArgAfter('/m')
   const fileName = name[0].toUpperCase() + name.slice(1).toLowerCase() + '.ts'
 
-  const textData = fs.readFileSync(path.join(paths.root, 'src', '_templates', 'model.ts')).toString()
-  let resultTextData = textData.replace(config.patternRegExp, (str) => replaceTemplate(str, name))
+  const textData = getTplText('model.ts')
+  let resultTextData = replaceAllTemplates(textData, name)
 
-  resultTextData = resultTextData.replace(
-    config.patternBlockMongoRegExp,
-    (str, content) => replaceTemplate(str, rawArgs.includes('-mongo') ? content : '')
-  )
+  // let resultTextData = textData.replace(config.patternRegExp, (str) => replaceTemplate(str, name))
 
-  console.log(rawArgs);
-  // console.log(resultTextData.trim());
+  resultTextData = replaceBlocksWithMongo(resultTextData)
 
-  fs.writeFileSync(
+  writeFileTo(
     path.join(paths.execRoot, 'src', 'models', fileName),
-    Buffer.from(resultTextData.trim())
+    resultTextData
+  )
+  // fs.writeFileSync(
+  //   path.join(paths.execRoot, 'src', 'models', fileName),
+  //   Buffer.from(resultTextData.trim())
+  // )
+}
+
+const generateService = () => {
+  const name = getArgAfter('/m')
+  const fileName = name[0].toUpperCase() + name.slice(1).toLowerCase() + 'Service.ts'
+
+  const textData = getTplText('service.ts')
+  let resultTextData = replaceAllTemplates(textData, name)
+
+  resultTextData = replaceBlocksWithMongo(resultTextData)
+
+  // resultTextData = resultTextData.replace(
+  //   config.patternBlockMongoRegExp,
+  //   (str, content) => replaceTemplate(str, rawArgs.includes('-mongo') ? content : '')
+  // )
+
+  writeFileTo(
+    path.join(paths.execRoot, 'src', 'services', fileName),
+    resultTextData
   )
 }
 
@@ -46,6 +91,9 @@ const generate = () => {
   }
   if (rawArgs.includes('/m')) {
     generateModel()
+  }
+  if (rawArgs.includes('/s')) {
+    generateService()
   }
 }
 
