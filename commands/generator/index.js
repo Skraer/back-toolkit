657f20ed85d5081d23777f5a384b8f32ec4309b7
Alexander Kizilov <kizilov.alex1823@gmail.com>
2022-10-25 12:56:05 +0300

    ...

--- commands/generator/index.js
+++ commands/generator/index.js
@@ -7,42 +7,62 @@ const { getArgAfter } = require('../../utils/getArgAfter')
 const { getTplText, replaceAllTemplates, expandBlocksWithArg, writeFileTo, getFileName } = require('./utils')
 const paths = require('../../paths')
 
+const handleTextData = (tplName, name) => {
+  let textData = getTplText(tplName)
+  textData = expandBlocksWithArg(textData)
+  textData = replaceAllTemplates(textData, name)
+  return textData
+}
+
+const generateController = (name) => {
+  const textData = handleTextData('controller.ts', name)
+  writeFileTo(
+    path.join(paths.execRoot, 'src', 'controllers', getFileName(name, 'Controller.ts')),
+    textData
+  )
+}
+
+const generateModel = (name) => {
+  const textData = handleTextData('model.ts', name)
+  writeFileTo(
+    path.join(paths.execRoot, 'src', 'models', getFileName(name, '.ts')),
+    textData
+  )
+}
+
+const generateService = (name) => {
+  const textData = handleTextData('service.ts', name)
+  writeFileTo(
+    path.join(paths.execRoot, 'src', 'services', getFileName(name, 'Service.ts')),
+    textData
+  )
+}
+
+const generateMiddleware = (name) => {
+  const textData = handleTextData('middleware.ts', name)
+  writeFileTo(
+    path.join(paths.execRoot, 'src', 'middlewares', getFileName(name, 'Middleware.ts')),
+    textData
+  )
+}
+
 const generate = () => {
   const fullArg = rawArgs.find(arg => arg.startsWith('gen'))
   const args = fullArg.split(':')
   const name = getArgAfter(fullArg)
 
-  let templateFileName, outputDirName, outputPostfix
-
   if (args.includes('c')) {
-    templateFileName = 'controller.ts'
-    outputDirName = 'controllers'
-    outputPostfix = 'Controller.ts'
+    generateController(name)
   }
   if (args.includes('m')) {
-    templateFileName = 'model.ts'
-    outputDirName = 'models'
-    outputPostfix = '.ts'
+    generateModel(name)
   }
   if (args.includes('s')) {
-    templateFileName = 'service.ts'
-    outputDirName = 'services'
-    outputPostfix = 'Service.ts'
+    generateService(name)
   }
   if (args.includes('mw')) {
-    templateFileName = 'middleware.ts'
-    outputDirName = 'middlewares'
-    outputPostfix = 'Middleware.ts'
+    generateMiddleware(name)
   }
-
-  let textData = getTplText(templateFileName)
-  textData = expandBlocksWithArg(textData)
-  textData = replaceAllTemplates(textData, name)
-
-  writeFileTo(
-    path.join(paths.execRoot, 'src', outputDirName, getFileName(name, outputPostfix)),
-    textData
-  )
 }
 
 module.exports = generate
