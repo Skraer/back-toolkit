a765a4285b46e3a4e03df67ebfe098519c49ebff
Alexander Kizilov <kizilov.alex1823@gmail.com>
2022-12-06 11:56:53 +0300

    ...

--- commands/utils/package.js
+++ commands/utils/package.js
@@ -1,15 +1,30 @@
 const getInstalledPackages = () => {
-  let arr = []
-  require('child_process').exec(
-    'npm ls --json',
-    function (err, stdout, stderr) {
-      if (err) return cb(err)
-      arr = stdout
-        ? Object.keys(JSON.parse(stdout)?.dependencies)
-        : []
-    }
-  )
-  return arr
+  // let arr = []
+  return new Promise((resolve) => {
+    require('child_process').exec(
+      'npm ls --json',
+      function (err, stdout, stderr) {
+        if (err) return cb(err)
+        arr = stdout
+          ? Object.keys(JSON.parse(stdout)?.dependencies)
+          : []
+        resolve(arr)
+        // console.log(1);
+      }
+    )
+  }).then(res => {
+    return res
+    // arr = res
+  })
+  // console.log(2);
+  // return arr
 }
+
+// getInstalledPackages().then(pkg => {
+//   console.log('PACKAGES >>>', pkg);
+// })
+
+// console.log('PACKAGES');
+// console.log('>>>', getInstalledPackages());
 
 exports.getInstalledPackages = getInstalledPackages
