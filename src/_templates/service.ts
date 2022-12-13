ddceda6f2c68ecf646823ee8a7080d8cd773fa19
Alexander Kizilov <kizilov.alex1823@gmail.com>
2022-12-13 16:35:33 +0300

    ...

--- src/_templates/service.ts
+++ src/_templates/service.ts
@@ -14,20 +14,12 @@ class /* TEMPLATE|T */Service {
   async getOnce(id: string) {
 
   }
+
   /* TEMPLATE_SWITCH
-  [-mongo]%async templateSwitch(id: string, data: Partial<Omit<I@TEMPLATE|T@, '_id'>>) {%
-  [!-mongo]%async templateSwitch(id: string, data: Partial<I@TEMPLATE|T@>) {%
-   */
+  [-mongo]%async update(id: string, data: Partial<Omit<I@TEMPLATE|T@, '_id'>>) {%
+  [!-mongo]%async update(id: string, data: Partial<I@TEMPLATE|T@>) {%
+  */
 
-  }
-
-  /* TEMPLATE_BLOCK[-mongo]>>async qwe(id: string, data: Partial<Omit<I@TEMPLATE|T@, '_id'>>) {*/
-  /* TEMPLATE_BLOCK[!-mongo]>>async qwe(id: string, data: Partial<I@TEMPLATE|T@>) {*/
-
-  }
-
-  async update(id: string, data: Partial</* TEMPLATE_BLOCK[-mongo]>>Omit<*/I/*TEMPLATE|T*//* TEMPLATE_BLOCK[-mongo]>>, '_id'> */>) {
-    
   }
 
   async delete(id: string) {
