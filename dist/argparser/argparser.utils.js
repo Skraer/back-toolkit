ecd9bcac5c77f4ba4798ee32a55e1bc7ad0213ec
Alexander Kizilov <kizilov.alex1823@gmail.com>
2023-06-08 13:13:57 +0300

    removed dist

--- dist/argparser/argparser.utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isModuleValid = exports.isGenValid = void 0;
-const isGenValid = (elem) => ['s', 'c', 'm', 'mw'].includes(elem);
-exports.isGenValid = isGenValid;
-const isModuleValid = (elem) => ['3rdparty', 'mongo', 'errh', 'auth'].includes(elem);
-exports.isModuleValid = isModuleValid;
--- dist/utils/getArgAfter.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getArgAfter = void 0;
-const rawArgs = process.argv.slice(2);
-const getArgAfter = (str) => rawArgs[rawArgs.findIndex((arg) => arg === str) + 1];
-exports.getArgAfter = getArgAfter;
